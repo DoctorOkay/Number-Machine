ca2df9fc65222c245d0ee9eff96fb79744b5b4ac
Dylan Vidas <dylan.vidas@gmail.com>
2021-03-31 00:22:43 -0500

    Initial commit

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField] private int minNumber;
+    [SerializeField] private int maxNumber;
+    [TextArea(10, 14)] [SerializeField] private string introText;
+    [SerializeField] private Text outputText;
+    [SerializeField] private Button[] guessButtons;
+
+    private int guess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        outputText.text = introText;
+        outputText.text = outputText.text + "Think of a number between " + minNumber + " and " + maxNumber + "\n";
+        outputText.text = outputText.text + "(Don't tell me what it is)\n";
+
+        MakeGuess();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ 
+    }
+
+    public void GuessTooHigh()
+    {
+        outputText.text = outputText.text + "Ahhh, my guess is too high.\n";
+        maxNumber = guess;
+        MakeGuess();
+    }
+
+    public void GuessTooLow()
+    {
+        outputText.text = outputText.text + "Hmmm, my guess is too low.\n";
+        minNumber = guess;
+        MakeGuess();
+    }
+
+    public void GuessCorrect()
+    {
+        outputText.text = outputText.text + "Great, I got it right!\n";
+        outputText.text = outputText.text + "Game over :)";
+
+        for(int i = 0; i < guessButtons.Length; i++)
+        {
+            guessButtons[i].interactable = false;
+        }
+    }
+
+    private void MakeGuess()
+    {
+        guess = (minNumber + maxNumber) / 2;
+        outputText.text = outputText.text + "I guess your number is: " + guess + "\n";
+    }
+}
