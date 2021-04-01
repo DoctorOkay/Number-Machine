00b06ae406c4e1ea903802b83bd5ddc2c70d1a19
Dylan Vidas <dylan.vidas@gmail.com>
2021-04-01 11:58:22 -0500

    Adding new files/assets to make the game work if you actually pull the code down

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    private Scene currentScene;
+
+    public void LoadGame()
+    {
+        currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene("NM_NumberMachine", LoadSceneMode.Single);
+        SceneManager.UnloadSceneAsync(currentScene.buildIndex);
+    }
+
+    public void LoadRules()
+    {
+        currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene("NM_Rules", LoadSceneMode.Single);
+        SceneManager.UnloadSceneAsync(currentScene.buildIndex);
+    }
+
+    public void LoadWin()
+    {
+        currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene("NM_GameOver", LoadSceneMode.Additive);
+        // SceneManager.UnloadSceneAsync(currentScene.buildIndex);
+    }
+
+    public void LoadMenu()
+    {
+        currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene("NM_MainMenu", LoadSceneMode.Single);
+        SceneManager.UnloadSceneAsync(currentScene.buildIndex);
+    }
+}
